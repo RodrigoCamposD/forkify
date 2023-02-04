6c15d6d07dd9fb7817f941fce70716ee25ba8954
RodrigoCamposD <campos6@gmail.com>
2023-02-04 14:16:46 -0300

    removing fractional module

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -27,34 +27,30 @@ export const AJAX = async function (url, uploadData = undefined) {
     throw error;
   }
 };
-/*
-export const getJSON = async function (url) {
-  try {
-    const fetchPro = fetch(url);
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    return data;
-  } catch (error) {
-    throw error;
-  }
-};
 
-export const sendJSON = async function (url, uploadData) {
-  try {
-    const fetchPro = fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(uploadData),
-    });
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    return data;
-  } catch (error) {
-    throw error;
+// function given by Kori in https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649681#questions/15600366
+export const numberToFraction = function (amount) {
+  // This is a whole number and doesn't need modification.
+  if (parseFloat(amount) === parseInt(amount)) return amount;
+  // Next lines are cribbed from https://stackoverflow.com/a/23575406.
+  const gcd = function (a, b) {
+    if (b < 0.0000001) return a;
+    return gcd(b, Math.floor(a % b));
+  };
+  const len = amount.toString().length - 2;
+  let denominator = Math.pow(10, len);
+  let numerator = amount * denominator;
+  const divisor = gcd(numerator, denominator);
+  numerator /= divisor;
+  denominator /= divisor;
+  let base = 0;
+  // In a scenario like 3/2, convert to 1 1/2
+  // by pulling out the base number and reducing the numerator.
+  if (numerator > denominator) {
+    base = Math.floor(numerator / denominator);
+    numerator -= base * denominator;
   }
+  amount = Math.floor(numerator) + "/" + Math.floor(denominator);
+  if (base) amount = base + " " + amount;
+  return amount;
 };
-*/
